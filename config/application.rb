be59b29327ea9fe28e036a6d0dc93e3714ecbea7
jlaprevote <jacklaprevote@gmail.com>
2018-08-17 19:55:27 -0600

    fix assets setting

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module ClickitCharlie
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.serve_static_assets = true
+    config.assets.enabled = true
   end
 end
