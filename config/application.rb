056ad96900af786ebfd8b59773de6a88a308d756
jlaprevote <jacklaprevote@gmail.com>
2018-08-26 13:49:17 -0600

    remove public/assets/

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module ClickitCharlie
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.serve_static_assets = true
-    config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
+
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # for sprockets_better_errors
+  config.assets.raise_production_errors = true
 end
